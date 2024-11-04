1b02efc8011fe39e0757dc73216f6525019565ad
karthik40404 <karpython123@gmail.com>
2024-11-04 15:40:29 +0530

    'stud'

--- prostudent/student/views.py
+++ prostudent/student/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 
 # Create your views here.
+
+std=[]
+
+def home(req):
+    if req.method=='POST':
+        roll=req.POST['roll_no']
+        name=req.POST['name']
+        age=req.POST['age']
+        classes=req.POST['classes']
+        std.append({'roll_no':roll,'name':name,'age':age,'classes':classes})
+        # print(std)
+        return redirect(home)
+    else:
+        return render(req,'home.html',{'students':std})
